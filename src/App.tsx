03f6085e65ac9565d963c31424134afdd271f253
Antoine Morin-Paulhus <antoine@lumen5.com>
2023-11-09 19:01:57 -0500

    initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,114 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useRef, useState } from 'react';
+import * as TWEEN from '@tweenjs/tween.js';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+let state = {
+    background: {
+        x: 10,
+        y: 10,
+        width: 800 - 20,
+        height: 400 - 20,
+        style: "rgba(220,220,220,1)",
+    },
+    text: {
+        offset: { x: 100, y: 30 },
+        lines: [
+            {
+                x: 0,
+                y: 50,
+                width: 400,
+                height: 40,
+                style: "rgba(100,100,100,1)",
+            },
+            {
+                x: 0,
+                y: 100,
+                width: 400,
+                height: 40,
+                style: "rgba(100,100,100,1)",
+            },
+            {
+                x: 0,
+                y: 150,
+                width: 300,
+                height: 40,
+                style: "rgba(100,100,100,1)",
+            }
+        ]
+    }
+};
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const drawRectangle = (
+    { ctx, x, y, width, height, style = "rgba(200,200,200,1)" }:
+        {
+            x: number,
+            y: number,
+            width: number,
+            height: number,
+            ctx: CanvasRenderingContext2D,
+            style: string,
+        }
+) => {
+    ctx.fillStyle = style;
+    ctx.fillRect(x, y, width, height);
+};
+
+const renderState = (
+    { ctx, state }:
+        {
+            ctx: CanvasRenderingContext2D,
+            state: any,
+        }
+) => {
+    drawRectangle({ ctx, ...state.background });
+    ctx.translate(state.text.offset.x, state.text.offset.y);
+    state.text.lines.map((line: any) => drawRectangle({ ctx, ...line }));
+};
+
+
+const drawCanvas = ({
+    time,
+    canvas,
+    ctx
+}: {
+    time: number,
+    canvas: HTMLCanvasElement,
+    ctx: CanvasRenderingContext2D
+}): void => {
+    ctx.resetTransform();
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    renderState({ ctx, state });
 }
 
-export default App
+function App() {
+    const canvasRef = useRef(null);
+    const [time, _setTime] = useState(0.5);
+
+    useEffect(() => {
+        if (!canvasRef.current) {
+            return;
+        }
+
+        let canvas: HTMLCanvasElement = canvasRef.current;
+        canvas.width = 800;
+        canvas.height = 400;
+
+        let ctx = canvas.getContext('2d');
+
+        if (!ctx) {
+            return;
+        }
+        drawCanvas({ time, canvas, ctx });
+    }, [canvasRef]);
+
+    return (
+        <>
+            <p className="">
+                Welcome to hackanton city.
+            </p>
+            <canvas ref={canvasRef} className="app-canvas"/>
+        </>
+    )
+}
+
+export default App;
